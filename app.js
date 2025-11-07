b2be2f9702f62957d4f2241b4fc5f66b2ae4eb14
JetProc <soonjae8297@gmail.com>
2025-11-08 00:05:07 +0900

    feat: implement github auth logic

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Precourse Badge Workshop Backend is running!');
 });
+
+const workshopRoutes = require('./routes/workshop');
+app.use('/api/workshop', workshopRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
