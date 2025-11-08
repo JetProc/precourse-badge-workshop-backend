aaa024a5b784dbc9f0560e9e03550761a8b73adf
jetproc <soonjae8297@gmail.com>
2025-11-08 15:47:39 +0900

    feat: add workshop and validation router

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const mongoose = require('mongoose');
-
-app.use(express.json());
-
+const authRouter = require('./routes/auth');
+const workshopRouter = require('./routes/workshop');
+const validationRouter = require('./routes/validation');
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
+app.use(express.json());
+
+app.use('/api/auth', authRouter);
+app.use('/api/workshop', workshopRouter);
+app.use('/api/validation', validationRouter);
+
 app.get('/', (req, res) => {
-  res.send('Precourse Badge Workshop Backend is running!');
+  res.status(200).send('Hello, Woowacourse Badge Workshop Backend!');
 });
 
-const authRoutes = require('./routes/auth');
-app.use('/api/auth', authRoutes);
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`Open in browser: http://localhost:${PORT}`);
 });
