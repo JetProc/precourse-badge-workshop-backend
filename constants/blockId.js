f96aa6b8e98f0385ae3a43bcd704f32a394ad8fc
JetProc <soonjae8297@gmail.com>
2025-11-10 18:01:58 +0900

    feat: add block ids

--- constants/blockId.js
+++ constants/blockId.js
@@ -17,9 +17,16 @@ const HIDDEN_INPUT_BLOCK_ID = '690fe1368d6d24478485b93f';
 const REVIEW_INPUT_BLOCK_ID = '69101e1d1ba0d6494f5fd30f';
 
 /*
- * 제작 관련 block
+ * 훈장 관련 block
  */
+//최종 결과 요청
 const SUBMIT_ORDER_BLOCK_ID = '69101f6966568549d0d275eb';
+//칭호 설정
+const SET_TITLE_BLOCK_ID = '6910a7f55eff070a5601856d';
+
+const GET_TITLE_BLOCK_ID = '69117530959f5c2e998aa89b';
+// 최종 훈장 확인 블록
+const SHOW_MY_BADGE_BLOCK_ID = '6910a80d1ba0d6494f5fddb4';
 
 module.exports = {
   START_AUTH_BLOCK_ID,
@@ -28,4 +35,7 @@ module.exports = {
   HIDDEN_INPUT_BLOCK_ID,
   REVIEW_INPUT_BLOCK_ID,
   SUBMIT_ORDER_BLOCK_ID,
+  SHOW_MY_BADGE_BLOCK_ID,
+  SET_TITLE_BLOCK_ID,
+  GET_TITLE_BLOCK_ID,
 };
